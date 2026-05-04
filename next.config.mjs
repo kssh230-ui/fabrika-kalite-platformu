e164684c5099d2e454c4f5118aadbc4821a7817a
kssh230-ui <kssh230@gmail.com>
2026-05-04 15:34:22 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  basePath: '/fabrika-kalite-platformu',
 };
 
 export default nextConfig;
